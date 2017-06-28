b51ac18170530c9bf72801b436c6c6062a33ed8e
178723651@qq.com <178723651@qq.com>
2017-06-28 15:28:41 +0800

    更新模版

--- gulp/tasks/javascripts.js
+++ gulp/tasks/javascripts.js
@@ -1,8 +1,16 @@
 
 var gulp = require('gulp');
 var config = require('../config').js;
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
 
 gulp.task('javascripts', function () {
   return gulp.src(config.src)
-      .pipe(gulp.dest(config.dest))
+      // .pipe(gulp.dest(config.dest))
+      .pipe(concat('main.js')).pipe(uglify()).pipe(gulp.dest('./dist/static/js'));
 });
+// gulp.task("javascripts",function(){
+//     // 把1.js和2.js合并压缩为main.js，输出到dest/js目录下
+//
+// })
